8997358d4483a085a900386062ef980ac5daa3de
XasanboyKarimov <xkarimov257@gmail.com>
2024-03-13 13:41:07 +0500

    edabit misollar3

---
+++ main.js
@@ -0,0 +1,121 @@
+//1.Leap Year Function ⌚
+//Write a function that returns true if a year is a leap, otherwise return false.
+
+//leapYear(1990) ➞ false
+
+// leapYear(1924) ➞ true
+
+// leapYear(2021) ➞ false
+
+// function leapYear(year) {
+// 	return year == 1
+// }
+//```
+
+//2.Return the Next Number from the Integer Passed
+//Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+// addition(0) ➞ 1
+
+// addition(9) ➞ 10
+
+// addition(-3) ➞ -2
+
+// function addition(num) {
+// 	return num +1
+// }
+//```
+
+//3.Binary Addition + 0 1 0 1
+//Create a function that takes two numbers and returns their sum as a binary string.
+
+//addBinary(1, 1) ➞ "10"
+
+// addBinary(1, 2) ➞ "11"
+
+// addBinary(4, 5) ➞ "1001"
+
+// function binary(decimal) {
+// 	return decimal * 0
+// }
+//```
+
+//4.Stack the Boxes
+//Here's an image of four models. Some of the cubes are hidden behind other cubes. Model one consists of one cube. Model two consists of four cubes, and so on
+
+// stackBoxes(1) ➞ 1
+
+// stackBoxes(2) ➞ 4
+
+// stackBoxes(0) ➞ 0
+
+//function stackBoxes(n) {
+// 	return n != n
+//  }
+
+//```
+
+//5.Less Than 100?
+//Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
+
+//lessThan100(22, 15) ➞ true
+// 22 + 15 = 37
+
+// lessThan100(83, 34) ➞ false
+// // 83 + 34 = 117
+
+// lessThan100(3, 77) ➞ true
+//```
+
+//6.Return the Remainder from Two Numbers
+// There is a single operator in JavaScript, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+
+// remainder(1, 3) ➞ 1
+
+// remainder(3, 4) ➞ 3
+
+// remainder(-9, 45) ➞ -9
+
+// remainder(5, 5) ➞ 0
+
+// function remainder(x, y) {
+// 	return x % y
+// }
+//```
+
+//7.Buggy Code (Part 1)
+// Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+// cubes(3) ➞ 27
+
+// cubes(5) ➞ 125
+
+// cubes(10) ➞ 1000
+
+// function a{
+// 	return a ** 3
+// }
+//```
+
+//8.Correct the Mistakes
+// Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+// squared(5) ➞ 25
+
+// squared(9) ➞ 81
+
+// squared(100) ➞ 10000
+
+//9.Sum of Polygon Angles
+// Given an n-sided regular polygon n, return the total sum of internal angles (in degrees).
+
+// sumPolygon(3) ➞ 180
+
+// sumPolygon(4) ➞ 360
+
+// sumPolygon(6) ➞ 720
+
+// function sumPolygon(n) {
+// 	return (n - 2) * 180
+// }
+// ```
